db3080f89c10122aebe93409acfa9f86523221f2
Julian Rubisch <julian@julianrubisch.at>
2020-01-21 21:28:02 +0100

    Remove Gemfile.lock

--- lib/attractor/javascript.rb
+++ lib/attractor/javascript.rb
@@ -1,9 +1,9 @@
 require "attractor/javascript/version"
 
 require "attractor/calculators/base_calculator"
-require "attractor/javascript/js_calculator"
+require "attractor/calculators/js_calculator"
 require "attractor/detectors/base_detector"
-require "attractor/javascript/js_detector"
+require "attractor/detectors/js_detector"
 
 module Attractor
   module Javascript
