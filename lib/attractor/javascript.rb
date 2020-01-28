ee9353892729c7428360444a0f2cb772062be0fd
Julian Rubisch <julian@julianrubisch.at>
2020-01-28 09:05:00 +0100

    Register detector and calculator

--- lib/attractor/javascript.rb
+++ lib/attractor/javascript.rb
@@ -1,5 +1,7 @@
 require "attractor/javascript/version"
 
+require "attractor"
+require "attractor/registry_entry"
 require "attractor/calculators/base_calculator"
 require "attractor/calculators/js_calculator"
 require "attractor/detectors/base_detector"
@@ -8,6 +10,7 @@ require "attractor/detectors/js_detector"
 module Attractor
   module Javascript
     class Error < StandardError; end
-    # Your code goes here...
+
+    Attractor.register(Attractor::RegistryEntry.new(type: "js", detector_class: JsDetector, calculator_class: JsCalculator))
   end
 end
